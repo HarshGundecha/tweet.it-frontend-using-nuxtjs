cfcf2bd9ac0bf224204838599e2262fc88cc9e72
HarshGundecha <harsh.gundecha@gmail.com>
2019-12-19 23:47:35 +0530

    add login & register pages

---
+++ pages/Login.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Legacy User Menu</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Legacy User Menu</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+
+    <section class="content">
+      <div class="row">
+        <div class="offset-md-5 col-md-4">
+          <div class="login-box">
+            <!-- /.login-logo -->
+            <div class="card">
+              <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+
+                <form @submit.prevent="login">
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="email"
+                      type="text"
+                      class="form-control"
+                      placeholder="Email"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="password"
+                      type="password"
+                      class="form-control"
+                      placeholder="Password"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <router-link
+                        to="/register"
+                        class="btn btn-danger btn-block"
+                      >
+                        <i class="fa fa-user-plus"></i> Register</router-link
+                      >
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-6">
+                      <button
+                        :disabled="isLoginBusy"
+                        type="submit"
+                        class="btn btn-success btn-block"
+                      >
+                        <span v-if="isLoginBusy">
+                          <span class="spinner-border spinner-border-sm"></span
+                          >&nbsp;loading...
+                        </span>
+                        <span v-else>Login</span>
+                      </button>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                </form>
+              </div>
+              <!-- /.login-card-body -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapGetters({ isLoginBusy: 'store/isLoginBusy' })
+  },
+  methods: {
+    login() {
+      const email = this.email
+      const password = this.password
+      this.$store
+        .dispatch('store/login', { email, password })
+        .then(() =>
+          this.$router.push(
+            '/profile/' + this.$store.getters['store/user'].username
+          )
+        )
+    }
+  }
+}
+</script>
---
+++ pages/Register.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="content-wrapper ">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Legacy User Menu</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Legacy User Menu</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+
+    <section class="content">
+      <div class="row">
+        <div class="offset-md-5 col-md-4">
+          <div class="register-box">
+            <div class="card">
+              <div class="card-body register-card-body">
+                <p class="login-box-msg">Register a new membership</p>
+
+                <form @submit.prevent="register">
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="username"
+                      type="text"
+                      class="form-control"
+                      placeholder="Username"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-user"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="name"
+                      type="text"
+                      class="form-control"
+                      placeholder="Name"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-user"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="email"
+                      type="email"
+                      class="form-control"
+                      placeholder="Email"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input
+                      v-model="password"
+                      type="password"
+                      class="form-control"
+                      placeholder="Password"
+                    />
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <router-link to="/login" class="btn btn-danger btn-block">
+                        <i class="fa fa-sign-in-alt"></i> Login</router-link
+                      >
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-6">
+                      <button
+                        :disabled="isRegistrationBusy"
+                        type="submit"
+                        class="btn btn-success btn-block"
+                      >
+                        <span v-if="isRegistrationBusy">
+                          <span class="spinner-border spinner-border-sm"></span
+                          >&nbsp;loading...
+                        </span>
+                        <span v-else>Register</span>
+                      </button>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                </form>
+              </div>
+              <!-- /.form-box -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Register',
+  data() {
+    return {
+      name: '',
+      username: '',
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapGetters({ isRegistrationBusy: 'store/isRegistrationBusy' })
+  },
+  methods: {
+    register() {
+      const data = {
+        name: this.name,
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }
+      this.$store
+        .dispatch('store/register', data)
+        .then(() => this.$router.push('/profile/' + data.username))
+    }
+  }
+}
+</script>
